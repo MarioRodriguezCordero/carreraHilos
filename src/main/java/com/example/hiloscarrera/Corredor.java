80ded5e71af9f492c0c8171dc6db95466c38039b
MarioRodriguezCordero <mario.rodcor@educa.jcyl.es>
2023-10-09 14:18:23 +0200

    Initial commit

---
+++ src/main/java/com/example/hiloscarrera/Corredor.java
@@ -0,0 +1,6 @@
+package com.example.hiloscarrera;
+
+public class Corredor {
+    private String nombre;
+    private long initialTime;
+}
---
+++ src/main/java/com/example/hiloscarrera/hilosCarreraController.java
@@ -0,0 +1,34 @@
+package com.example.hiloscarrera;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+
+public class hilosCarreraController {
+
+    @FXML
+    private Button btnIniciar;
+
+    @FXML
+    private Label ganador;
+
+    @FXML
+    private ProgressBar probarCorredor1;
+
+    @FXML
+    private ProgressBar probarCorredor2;
+
+    @FXML
+    private ProgressBar probarCorredor3;
+
+    @FXML
+    private ProgressBar probarCorredor4;
+
+    @FXML
+    void iniciar(ActionEvent event) {
+
+    }
+
+}
