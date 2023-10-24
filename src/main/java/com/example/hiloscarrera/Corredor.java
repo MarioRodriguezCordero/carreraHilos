c0a4e5c016fcec7901b2b6126d47cd5c125a5f18
MarioRodriguezCordero <mario.rodcor@educa.jcyl.es>
2023-10-24 11:09:23 +0200

    Cambios

--- src/main/java/com/example/hiloscarrera/Corredor.java
+++ src/main/java/com/example/hiloscarrera/Corredor.java
@@ -1,6 +1,45 @@
 package com.example.hiloscarrera;
 
-public class Corredor {
+public class Corredor extends Thread{
     private String nombre;
+    private int velocidad;
     private long initialTime;
+
+    public Corredor() {
+    }
+
+    public Corredor(String nombre, int velocidad, long initialTime) {
+        this.nombre = nombre;
+        this.velocidad = velocidad;
+        this.initialTime = initialTime;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(int velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public long getInitialTime() {
+        return initialTime;
+    }
+
+    public void setInitialTime(long initialTime) {
+        this.initialTime = initialTime;
+    }
+
+    @Override
+    public void run(){
+
+    }
 }
--- src/main/java/com/example/hiloscarrera/hilosCarreraController.java
+++ src/main/java/com/example/hiloscarrera/hilosCarreraController.java
@@ -3,8 +3,10 @@ package com.example.hiloscarrera;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
+
+import static java.lang.System.currentTimeMillis;
 
 public class hilosCarreraController {
 
@@ -12,7 +14,7 @@ public class hilosCarreraController {
     private Button btnIniciar;
 
     @FXML
-    private Label ganador;
+    private TextField ganador;
 
     @FXML
     private ProgressBar probarCorredor1;
@@ -28,6 +30,13 @@ public class hilosCarreraController {
 
     @FXML
     void iniciar(ActionEvent event) {
+        long initialTime = System.currentTimeMillis();
+
+        Corredor corredor1 = new Corredor("Juan", 3, initialTime);
+        Corredor corredor2 = new Corredor("Alberto", 5, initialTime);
+        Corredor corredor3 = new Corredor("Jose", 1, initialTime);
+        Corredor corredor4 = new Corredor("Mario", 2, initialTime);
+
 
     }
 
